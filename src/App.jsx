aa3522f6039e0b4417e7a1a738c357c532700e9c
Luka Noza <103760971+LukaNoza@users.noreply.github.com>
2025-12-02 17:14:37 +0400

    s

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import header from "./components/header";
+
+export default function App() {
+  return (
+    <main>
+      {/*gradient image*/}
+      <img
+        className="absolute top-0 right-0 opacity-60 -z-1"
+        src="/gradient.png"
+        alt="Gradient-img"
+      />
+      <div
+        className="h-0 w-[40rem] absolute top-[20%] right-[-5%]
+      shadow-[0_0_900px_20px_#e99b63] -rotate-[30deg] -z-10"
+      ></div>
+      <header />
+    </main>
+  );
+}
---
+++ src/components/header.jsx
@@ -0,0 +1,26 @@
+const header = () => {
+  return (
+    <header
+      className="flex justify-between 
+  items-center py-4 px-4 lg:px-20"
+    >
+      <h1
+        className="text-3xl md:text-4xl lg:text-5xl
+      font-light m-0"
+      >
+        LCODE
+      </h1>
+      <nav>
+        <a
+          className="text-base tracking-wider
+        transition-colors hover:text-gray-300 z-50"
+          href="#"
+        >
+          Company
+        </a>
+      </nav>
+    </header>
+  );
+};
+
+export default header;
