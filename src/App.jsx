9f033dd4d1c63232dc8a65c9e361e6c416db8aae
Luka Noza <103760971+LukaNoza@users.noreply.github.com>
2025-12-10 18:17:07 +0400

    export final product

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
-import header from "./components/header";
+import "boxicons/css/boxicons.min.css";
+import Header from "./components/header";
+import Hero from "./components/Hero";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
 
 export default function App() {
+  useEffect(() => {
+    AOS.init({
+      duration: 1500,
+      once: true,
+    });
+  });
   return (
     <main>
-      {/*gradient image*/}
+      {/* gradient image */}
       <img
         className="absolute top-0 right-0 opacity-60 -z-1"
         src="/gradient.png"
@@ -11,9 +22,10 @@ export default function App() {
       />
       <div
         className="h-0 w-[40rem] absolute top-[20%] right-[-5%]
-      shadow-[0_0_900px_20px_#e99b63] -rotate-[30deg] -z-10"
+        shadow-[0_0_900px_20px_#e99b63] -rotate-[30deg] -z-10"
       ></div>
-      <header />
+      <Header /> {/* ← USE IT LIKE THIS */}
+      <Hero />
     </main>
   );
 }
